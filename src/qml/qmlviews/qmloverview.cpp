6d7086536317300aceb183106f20a29e1bf102c2
Daniel Lindenfelser <daniel@lindenfelser.de>
2013-12-15 09:20:05 +0100

    prepare waveform qml items

---
+++ src/qml/qmlviews/qmloverview.cpp
@@ -0,0 +1,1 @@
+#include "qmlviews/qmloverview.h"
---
+++ src/qml/qmlviews/qmloverview.h
@@ -0,0 +1,14 @@
+#ifndef QMLOVERVIEW_H
+#define QTQUICK1_H
+
+#include <QQuickItem>
+
+class QtQuick1 : public QQuickItem
+{
+    Q_OBJECT
+public:
+	
+};
+
+#endif // QMLOVERVIEW_H
+
